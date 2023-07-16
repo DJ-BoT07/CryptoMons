3f809df874818714c0069d9b46f6a081638d99e6
DJ-BoT07 <119739412+DJ-BoT07@users.noreply.github.com>
2023-07-16 23:10:40 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
         <Link to='/'>
             <div className='navbar'>
                 <FaCoins className='icon' />
-                <h1> Coin <span className='purple'>Search</span></h1>
+                <h1> Crypto <span className='purple'>Mon</span></h1>
             </div>
         </Link>
     )
